4efa04e0475651b9b90eb12e54d3a141a1f1c545
Alexander Nikitin <alexander.nikitin@quantumsoft.ru>
2021-08-04 13:06:40 +0700

    Add high DPI support.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
     core::MonitoringTool mtool;
     return a.exec();
