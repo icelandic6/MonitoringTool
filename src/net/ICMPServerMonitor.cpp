3efce95817292feaf8e1e6730e6873b6142ec583
Alexander Nikitin <alexander.nikitin@quantumsoft.ru>
2021-08-04 21:04:33 +0700

    Add resolving hostname to ipv4 address for ICMP protocol monitor

--- src/net/ICMPServerMonitor.cpp
+++ src/net/ICMPServerMonitor.cpp
@@ -1,16 +1,20 @@
 #include "ICMPRequestWorker.h"
 #include "ICMPServerMonitor.h"
 
-#include <QtNetwork/QTcpSocket>
 #include <QTimer>
 #include <QThread>
-
+#include <QHostInfo>
 
 class ICMPServerMonitorPrivate : public QObject
 {
     Q_DECLARE_PUBLIC(ICMPServerMonitor)
     Q_DISABLE_COPY(ICMPServerMonitorPrivate)
     ICMPServerMonitor *q_ptr = nullptr;
+
+    QHostAddress _ipv4Address;
+    bool _useResolving = false;
+    ushort _ipv4AddressUsages = 0;
+    const ushort _ipv4AddressUsageMax = 60;
 
 public:
     explicit ICMPServerMonitorPrivate(ICMPServerMonitor *q)
@@ -19,13 +23,40 @@ public:
     }
 
     ~ICMPServerMonitorPrivate() = default;
+
+    void hostLookedUp(QHostInfo info)
+    {
+        _ipv4Address = info.addresses().first();
+    }
+
+    void checkIPv4AddressUsage()
+    {
+        if (_ipv4AddressUsages >= _ipv4AddressUsageMax)
+        {
+            Q_Q(ICMPServerMonitor);
+            QHostInfo::lookupHost(q->address(), this, &ICMPServerMonitorPrivate::hostLookedUp);
+            _ipv4AddressUsages = 0;
+        }
+        else
+            _ipv4AddressUsages++;
+    }
 };
 
 ICMPServerMonitor::ICMPServerMonitor(const QString &address, QObject *parent)
     : ServerMonitor(address, parent)
     , d_ptr(new ICMPServerMonitorPrivate(this))
 {
-    qDebug() << QString("====== ICMPServerMonitor: host address = [%1]").arg(address);
+    Q_D(ICMPServerMonitor);
+
+    qDebug() << QString("\n====== ICMPServerMonitor: host address = [%1]\n").arg(address);
+
+    d->_ipv4Address = QHostAddress(address);
+
+    if (d->_ipv4Address.isNull())
+    {
+        d->_useResolving = true;
+        QHostInfo::lookupHost(address, d, &ICMPServerMonitorPrivate::hostLookedUp);
+    }
 }
 
 ICMPServerMonitor::~ICMPServerMonitor() = default;
@@ -36,9 +67,15 @@ void ICMPServerMonitor::checkServer()
 
     qDebug() << "\n====== Running ICMP server check ======";
 
+    if (d->_ipv4Address.isNull())
+    {
+        emit finished(false);
+        return;
+    }
+
     auto _requestThread = new QThread();
 
-    auto _requestWorker = new ICMPRequestWorker(address());
+    auto _requestWorker = new ICMPRequestWorker(d->_ipv4Address.toString());
     _requestWorker->moveToThread(_requestThread);
 
     connect(_requestThread, &QThread::started, _requestWorker, &ICMPRequestWorker::send_request);
@@ -46,11 +83,13 @@ void ICMPServerMonitor::checkServer()
 
     connect(_requestWorker, &ICMPRequestWorker::ready, _requestThread, &QThread::quit);
     connect(_requestWorker, &ICMPRequestWorker::ready, _requestWorker, &QObject::deleteLater);
-
     connect(_requestWorker, &ICMPRequestWorker::ready, this, [this](bool success, int latency)
     {
         emit finished(success, latency);
     });
 
     _requestThread->start();
+
+    if (d->_useResolving)
+        d->checkIPv4AddressUsage();
 }
